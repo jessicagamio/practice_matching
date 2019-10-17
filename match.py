25846f8cf3da90783d3e1642d7156cd955fc1571
jessica.gamio@gmail.com <jessica.gamio@gmail.com>
2019-10-17 15:09:10 -0700

    added middle initial problem

--- match.py
+++ match.py
@@ -4,13 +4,13 @@ def name_match(aliases,given):
     """return True if name matches alias.
 
 
-        >>> known_aliases = ['Alphonse Capone']
-        >>> name_match(known_aliases, 'Alphonse Gabriel Capone')
+        >>> known_aliases = ['Alphonse Gabriel Capone', 'Alphonse F Capone']
+        >>> name_match(known_aliases, 'Alphonse G Capone')
         True
-        >>> name_match(known_aliases, 'Alexander Capone')
+        >>> name_match(known_aliases, 'Alphonse E Capone')
         False
-        >>> name_match(known_aliases, 'Alphonse Francis Capone')
-        True 
+        >>> name_match(known_aliases, 'Alphonse Gregory Capone')
+        False 
 
     """
 
@@ -19,52 +19,52 @@ def name_match(aliases,given):
     if given in aliases:
         return True
 
-    # split given int a list by spaces in given string
-    giv_name = given.split(" ")
-    print(giv_name)
+    else:
 
-    # assign giv_first to first name and giv_last to last name
-    giv_first = giv_name[0]
-    giv_last = giv_name[-1]
+        # split given int a list by spaces in given string
+        giv_name = given.split(" ")
 
-    # if given name has a middle name assign giv_mid to middle name 
-    if len(giv_name) > 2:
-        giv_mid = giv_name[1:-1][0]
-    
-    print("given_name====>", giv_first, giv_last)
+        # assign giv_first to first name and giv_last to last name
+        giv_first = giv_name[0]
+        giv_last = giv_name[-1]
 
-    if aliases:
+        # if given name has a middle name assign giv_mid to middle name 
+        if len(giv_name) > 2:
+            giv_mid = giv_name[1:-1][0]
+  
         # split each name in aliases. Assign first_name and last_name
-        for name in aliases:
-            name=name.split(" ")
-            first_name=name[0]
-            # print('first name---->', first_name)
-            last_name=name[-1]
-            # print('last name----->', last_name)
-
-            # if middle name exissts assign mid_name
-            if len(name)>2:
-                mid_name=name[1:-1][0]
-                # print('middle name----->', mid_name)
+        for alias in aliases:
+            alias = alias.split(" ")
+            
+            alias_first = alias[0]
+            alias_last = alias[-1]
+            
+            # if middle name exists assign mid_name, else it is None
+            if len(alias)>2:
+                alias_mid = alias[1:-1][0]
             else:
-                mid_name=None
+                alias_mid=None
 
-            # if alias name matches given name
-            if first_name == giv_first and last_name == giv_last:
+            # if alias name matches given name, confirm middle name
+            if alias_first == giv_first and alias_last == giv_last:
+                
+                # if middle name exists for given and alias name compare them
                 if len(giv_name)>2:
-                    # print("you should be here!!!!")
-                    if mid_name:
-                        return giv_mid == mid_name
-                    return True
+                    if len(alias_mid)>1 and len(giv_mid)>1:
+                        return giv_mid == alias_mid
+                    elif len(alias_mid)==1 and len(giv_mid)>1:
+                        return alias_mid == giv_mid[0]
+                    elif len(alias_mid)>1 and len(giv_mid)==1:
+                        return alias_mid[0] == giv_mid
+                return True
 
         return False
 
-known_aliases = ['Alphonse Gabriel Capone', 'Alphonse Francis Capone']
-answer=name_match(known_aliases, 'Alphonse Gabriel Capone')
-print(answer)
 
-# if __name__=="__main__":
-#     from doctest import testmod
-#     if testmod().failed == 0:
-#         print(" **** Correct **** ")
+
+
+if __name__=="__main__":
+    from doctest import testmod
+    if testmod().failed == 0:
+        print(" **** Correct **** ")
 

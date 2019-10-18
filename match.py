c6ce13227ffc5350e150878de135a50a0847e337
jessica.gamio@gmail.com <jessica.gamio@gmail.com>
2019-10-17 17:04:40 -0700

    add additional functions to clean up code

--- match.py
+++ match.py
@@ -1,61 +1,82 @@
 
+def middle_evaluate(alias_mid, giv_mid):
+    """evaluate middle name with or without initials"""
+
+    if len(alias_mid)>1 and len(giv_mid)>1:
+        return giv_mid == alias_mid
+
+    elif len(alias_mid)==1 and len(giv_mid)>1:
+        return alias_mid == giv_mid[0]
+
+    elif len(alias_mid)>1 and len(giv_mid)==1:
+        return alias_mid[0] == giv_mid
+
+def split_name(name):
+
+    name = name.split(" ")
+
+    # assign first and last from name list
+    first = name[0]
+    last = name[-1]
+
+    # if given name has over 2 names assign mid 
+    if len(name) > 2:
+        mid = name[1:-1][0]
+    else:
+        mid = None
+    return (first,mid,last)
+
 
 def name_match(aliases,given):
     """return True if name matches alias.
 
 
-        >>> known_aliases = ['Alphonse Gabriel Capone', 'Alphonse F Capone']
-        >>> name_match(known_aliases, 'Alphonse G Capone')
+        >>> known_aliases = ['Alphonse Gabriel Capone']
+        >>> name_match(known_aliases, 'Gabriel Alphonse Capone')
         True
-        >>> name_match(known_aliases, 'Alphonse E Capone')
+        >>> name_match(known_aliases, 'Gabriel A Capone')
+        True
+        >>> name_match(known_aliases, 'Gabriel Capone')
+        True
+        >>> name_match(known_aliases, 'Gabriel Francis Capone')
         False
-        >>> name_match(known_aliases, 'Alphonse Gregory Capone')
-        False 
+        >>>
 
     """
 
-    
     # if given name matches exactly with alias return True
     if given in aliases:
         return True
 
     else:
-
-        # split given int a list by spaces in given string
-        giv_name = given.split(" ")
-
-        # assign giv_first to first name and giv_last to last name
-        giv_first = giv_name[0]
-        giv_last = giv_name[-1]
-
-        # if given name has a middle name assign giv_mid to middle name 
-        if len(giv_name) > 2:
-            giv_mid = giv_name[1:-1][0]
+        # split given name into a list
+        giv_first, giv_mid, giv_last = split_name(given)
   
-        # split each name in aliases. Assign first_name and last_name
+        # split each name in aliases.
         for alias in aliases:
-            alias = alias.split(" ")
-            
-            alias_first = alias[0]
-            alias_last = alias[-1]
-            
-            # if middle name exists assign mid_name, else it is None
-            if len(alias)>2:
-                alias_mid = alias[1:-1][0]
-            else:
-                alias_mid=None
+            alias_first, alias_mid, alias_last = split_name(alias)
 
-            # if alias name matches given name, confirm middle name
-            if alias_first == giv_first and alias_last == giv_last:
+            # if middle name exists evaluate name with middle name
+            if giv_mid:
+                # if alias name matches given name, confirm middle name
+                if alias_first == giv_first and alias_last == giv_last:
+                    return middle_evaluate(alias_mid, giv_mid)
+             
+                # if name is transposed 
+                elif alias_first == giv_mid and alias_mid == giv_first:
+                    # switch first and middle name of given
+                    temp = giv_first
+                    giv_first = giv_mid
+                    giv_mid = temp
+
+                    return middle_evaluate(alias_mid, giv_mid)
                 
-                # if middle name exists for given and alias name compare them
-                if len(giv_name)>2:
-                    if len(alias_mid)>1 and len(giv_mid)>1:
-                        return giv_mid == alias_mid
-                    elif len(alias_mid)==1 and len(giv_mid)>1:
-                        return alias_mid == giv_mid[0]
-                    elif len(alias_mid)>1 and len(giv_mid)==1:
-                        return alias_mid[0] == giv_mid
+                # if name is transposed with initial
+                elif alias_first[0] == giv_mid and alias_mid == giv_first:
+                    return True   
+
+            # if given name only includes middle and last name
+            elif alias_mid == giv_first and alias_last == giv_last:
                 return True
 
         return False
